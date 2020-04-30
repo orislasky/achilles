fb34d4cba6afe76c37ef1306c6451ba5e68ca555
EC2 Default User <ec2-user@ip-172-31-0-175.us-east-2.compute.internal>
2020-04-30 09:14:41 +0000

    Fist version of my tool

---
+++ achilles.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import validators
+import requests
+from bs4 import BeautifulSoup
+from bs4 import Comment
+from urllib.parse import urlparse
+import yaml
+
+
+parser = argparse.ArgumentParser(description='The Achilles HTML Vulnerability Analyzer Version 1.0')
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('url', type=str, help="The URL of the HTML to analyze")
+parser.add_argument('--config', help='Path to configuration file')
+
+args = parser.parse_args()
+report = ''
+url = args.url
+
+
+config = {'forms': True, 'comments': True, 'passwords': True}
+ 
+if(args.config):
+  print('Using config file: ' + args.config)
+  config_file = open(args.config, 'r')
+  config_from_file = yaml.load(config_file)
+  if(config_from_file):
+    config = config_from_file
+ 
+if(validators.url(url)):
+  result_html = requests.get(url).text
+  parsed_html = BeautifulSoup(result_html, 'html.parser')
+  forms= parsed_html.find_all('form')
+  comments= parsed_html.find_all(string=lambda text:isinstance(text,Comment))
+  password_inputs = parsed_html.find_all('input', { 'name' : 'password'})
+  if(config['forms']):
+    for form in forms:
+      if((form.get('action').find('https') < 0) and (urlparse(url).scheme != 'https')):
+        report += 'Form Issue: Insecure form action ' + form.get('action') + ' found in document\n'
+  if(config['comments']):
+    for comment in comments:
+      if(comment.find('key: ') > -1):
+        report += 'Comment Issue: Key is found in the HTML comments, please remove\n'
+  if(config['passwords']):
+    for password_input in password_inputs:
+      if(password_input.get('type') != 'password'):
+        report += 'Input Issue: Plaintext password input found. Please change to password type input\n'
+else:
+  print('Invalud URL. Please include full URL including scheme.')
+
+
+if(report == ''):
+  print('Nice job! Your HTML document is secure!')
+else:
+  print('Vulnerability Report is as follows:\n')
+  print('==================================\n\n')
+  print(report)
+
+
